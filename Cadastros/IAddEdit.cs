94168641513f3f7fba4d17141fbaeafcf8b2cb91
fmartinazzo <fmartinazzo@intuitive.inf.br>
2012-05-08 15:47:19 -0300

    Novas Interfaces
    Signed-off-by: fmartinazzo <fmartinazzo@intuitive.inf.br>

---
+++ Cadastros/IAddEdit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TelasControllers
+{
+    public interface IAddEdit
+    {
+        ActionResult Add();
+        ActionResult Add(FormCollection collection);
+        ActionResult Edit(string keys);
+        ActionResult Edit(string keys, FormCollection collection);
+        ActionResult Delete(string keys, Boolean certeza);
+        bool BusinessValidator<T>(FormCollection collection, ref T objTable) where T : class;
+    }
+}
---
+++ Cadastros/IEdit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TelasControllers
+{
+    public interface IEdit
+    {
+        ActionResult List();
+        ActionResult Edit(string keys);
+        ActionResult Edit(string keys, FormCollection collection);
+        bool BusinessValidator<T>(FormCollection collection, ref T objTable) where T : class;
+    }
+}
